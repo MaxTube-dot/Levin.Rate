c765b02d8c038e58d1d7c150dfc2b0532b648469
Андрей <andreifarafonow@mail.ru>
2021-11-26 21:55:54 +0300

    Создан пустой проект.Добавьте файлы проекта.

---
+++ Levin.Rate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Levin.Rate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
